76db163b90ea98e4a9b1b2090490f71bb43bee40
Treidex <telkids4@gmail.com>
2020-08-16 13:42:29 -0700

    Player Completion

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Camera mainCamera;
+    public float speed;
+    [Range(0, 1)]
+    public float moveSpeed;
+    public float mouseY;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            mouseY = Camera.main.ScreenToWorldPoint(new Vector2(0, Camera.main.pixelHeight - Input.GetTouch(0).position.y)).y;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            FollowMouse(Input.mousePosition);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move(new Vector2(speed, 0));
+        Move(new Vector2(0, Mathf.Clamp(mouseY - transform.position.y, -moveSpeed, moveSpeed)));
+    }
+
+    private void Move(Vector2 dir)
+    {
+        rb.MovePosition(rb.position + dir);
+    }
+
+    private void FollowMouse(Vector2 mouse)
+    {
+        Vector2 worldMouse = mainCamera.ScreenToWorldPoint(mouse);
+        mouseY = worldMouse.y;
+    }
+}
