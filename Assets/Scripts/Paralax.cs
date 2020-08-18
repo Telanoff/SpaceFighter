cbae1f0e66fd85674eeb03571853217fe632e3cf
Treidex <telkids4@gmail.com>
2020-08-17 21:31:07 -0700

    Paralx

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    private float startPos;
+    private float dist;
+    private float width;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+
+        startPos = transform.position.x;
+        width = sprite.bounds.size.x;
+    }
+
+    private void FixedUpdate()
+    {
+        dist -= GameManager.instance.PlayerSpeed;
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        if (dist < startPos - width)
+            dist += width;
+    }
+}
