2396200912f9148854d235a7e188126000b2e5ed
Treidex <telkids4@gmail.com>
2020-09-03 21:06:00 -0700

    Added Run Summary and fixed visual bug

---
+++ Assets/Scripts/Player/RunSummary.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class RunSummary : MonoBehaviour
+{
+    public GameObject panel;
+    public TextMeshProUGUI scoreTMP;
+    public TextMeshProUGUI debrisTMP;
+    public TextMeshProUGUI starDustTMP;
+
+    public void ShowPanel()
+    {
+        panel.SetActive(true);
+    }
+
+    public void DisplayItems()
+    {
+        scoreTMP.SetText($"Score: {(int) GameManager.instance.Player.distance}");
+        debrisTMP.SetText($"Debris: {GameManager.instance.DebrisCount}");
+        starDustTMP.SetText($"Star Dust: {GameManager.instance.StarCount}");
+    }
+}
